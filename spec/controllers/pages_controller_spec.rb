4ad844ce2935ade7acd1c3c04b3794b971107535
Philip Arndt <parndt@gmail.com>
2011-01-18 15:36:40 +1300

    use factory girl

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,17 +4,21 @@ describe PagesController, "check pages and user roles for permissibility" do
   before do
     @page = Page.create!(:title => 'my page')
     @page.roles << Role[:my_role]
-    @user = mock(User)
+    # ensure refinery user exists
+    @refinery_user = Factory(:refinery_user)
+    # mock our user
+    @user = Factory(:user)
   end
 
   it "should 404 for invalid page/user role combination" do
-    current_user = @user.stub(:roles).and_return([])
+    current_user = @user
     get :show, :id => @page.id
     response.status.should eql 404
   end
 
   it "should 200 for valid page/user role combination" do
-    current_user = @user.stub(:roles).and_return([Role[:my_role]])
+    current_user = @user
+    current_user.roles << Role[:my_role]
     get :show, :id => @page.id
     response.status.should eql 200
   end

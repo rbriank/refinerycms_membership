67b828d77cd9798d23369f38fd656a67f386ac6e
Angelo Grossini <angelo@intercom.it>
2011-04-05 18:10:14 +0200

    Fix redirect in some cases

--- lib/memberships/warden_failure.rb
+++ lib/memberships/warden_failure.rb
@@ -6,6 +6,19 @@ module Refinery
         return "MembersController".constantize.action('login') unless params[:member_login].blank?
         return "#{controller.camelize}Controller".constantize.action(action) if params[:member_login].blank?
       end
+      
+      def redirect_url
+        if request_format == :html 
+          if /^admin/.match(params[:controller])
+            send(:"new_#{scope}_session_path") 
+          else
+            login_members_path(:redirect => request.fullpath, :member_login => true)
+          end
+        else
+          send(:"new_#{scope}_session_path", :format => request_format)
+        end
+      end
+
     end
   end
 end

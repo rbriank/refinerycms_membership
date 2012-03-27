1c32ad4925295a1d53905a45a28355abd0e446d9
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-03-27 13:25:09 -0700

    namespace and move around route definitions

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,49 @@
 Refinery::Core::Engine.routes.draw do
+  scope :module => :memberships do
 
-
-  namespace :memberships, :path => '' do
-    namespace :admin, :path => 'refinery' do
-      scope :path => 'memberships' do
-
-        resources :memberships, :only => :index do
-          collection do
-            get :settings
-            put :save_settings
-          end
-        end
-        resources :roles
-        resources :members do
-          member do
-            put :extend
-
-            put :enable
-            put :disable
-
-            put :accept
-            put :reject
-          end
-        end
-        resources :membership_emails, :except => :show do
-          collection do
-            get :settings
-            put :save_settings
-          end
-        end
-        resources :membership_email_parts, :except => :index
-
-      end
+    # Frontend routes
+    resource :members, :except => [:destroy] do
+      get :login
+      #match '/new/welcome' => 'members#welcome', :as => :welcome
+      get :welcome
+      get :new_password
+      match '/new_password' => 'members#create_password', :as => :create_password
+      match '/new_password/created' => 'members#new_password_created', :as => :new_password_created
+      get :profile
+      match '/activate/:confirmation_token' => 'members#activate', :as => :activate, :constraints => {:confirmation_token => /[a-zA-Z0-9]+/}, :via => :get
     end
-  end
 
-  resource :members, :except => [:destroy] do
-    get :login
-		#match '/new/welcome' => 'members#welcome', :as => :welcome
-		get :welcome
-		get :new_password
-		match '/new_password' => 'members#create_password', :as => :create_password
-		match '/new_password/created' => 'members#new_password_created', :as => :new_password_created
-    get :profile
-    match '/activate/:confirmation_token' => 'members#activate', :as => :activate, :constraints => {:confirmation_token => /[a-zA-Z0-9]+/}, :via => :get
-	end
+    # Admin routes
+    namespace :admin, :path => 'refinery/memberships' do
+      resources :memberships, :only => :index do
+        collection do
+          get :settings
+          put :save_settings
+        end
+      end
+
+      resources :roles
+      resources :members do
+        member do
+          put :extend
+
+          put :enable
+          put :disable
+
+          put :accept
+          put :reject
+        end
+      end
+
+      resources :membership_emails, :except => :show do
+        collection do
+          get :settings
+          put :save_settings
+        end
+      end
+
+      resources :membership_email_parts, :except => :index
+    end
+
+  end
 end

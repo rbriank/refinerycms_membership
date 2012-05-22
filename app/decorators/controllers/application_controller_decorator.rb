73093eebdafddc5e29ce0bfaa33a6ffcb502aa1f
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-05-22 16:18:39 -0700

    removed remainign monkeypatches. Pages uses before_filter for permissions

--- app/decorators/controllers/application_controller_decorator.rb
+++ app/decorators/controllers/application_controller_decorator.rb
@@ -1,32 +1,32 @@
 #redirect user to the right page after login
 ApplicationController.class_eval do
-  def render(*args)
-    unless @page.nil? || self.class.name == 'PagesController' || self.class.name =~ /^Admin::/
-      redirect_to login_members_path(:redirect => env['REQUEST_URI'], :member_login => true) unless @page.user_allowed?(current_refinery_user)
-      super *args if @page.user_allowed?(current_refinery_user)
-    else
-      super *args
-    end
-  end
+  # def render(*args)
+  #   unless @page.nil? || self.class.name == 'PagesController' || self.class.name =~ /^Admin::/
+  #     redirect_to login_members_path(:redirect => env['REQUEST_URI'], :member_login => true) unless @page.user_allowed?(current_refinery_user)
+  #     super *args if @page.user_allowed?(current_refinery_user)
+  #   else
+  #     super *args
+  #   end
+  # end
 
-  protected
-  def after_sign_in_path_for(resource_or_scope)
+  # protected
+  # def after_sign_in_path_for(resource_or_scope)
 
-    # if resource_or_scope.class.superclass.name == 'User' ||
-      # resource_or_scope.class.name == 'User' ||
-      # resource_or_scope.to_s == 'user'
-    if (resource_or_scope.instance_of?(Refinery::User) || resource_or_scope == :user)
-      if params[:redirect].present?
-        params[:redirect]
-      else
-        if !resource_or_scope.is_a?(Symbol) && (resource_or_scope.has_role?('Superuser')||resource_or_scope.has_role?('Refinery'))
-          super
-        else
-          '/'
-        end
-      end
-    else
-      super
-    end
+  #   # if resource_or_scope.class.superclass.name == 'User' ||
+  #     # resource_or_scope.class.name == 'User' ||
+  #     # resource_or_scope.to_s == 'user'
+  #   if (resource_or_scope.instance_of?(Refinery::User) || resource_or_scope == :user)
+  #     if params[:redirect].present?
+  #       params[:redirect]
+  #     else
+  #       if !resource_or_scope.is_a?(Symbol) && (resource_or_scope.has_role?('Superuser')||resource_or_scope.has_role?('Refinery'))
+  #         super
+  #       else
+  #         '/'
+  #       end
+  #     end
+  #   else
+  #     super
+  #   end
   end
 end
--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,20 +1,28 @@
 ::Refinery::PagesController.class_eval do
-  def show
-    # Find the page by the newer 'path' or fallback to the page's id if no path.
-    @page = Refinery::Page.find(params[:path] ? params[:path].to_s.split('/').last : params[:id],
-      :include => :roles)
+  before_filter :restrict_by_role, :only => :show
 
-    if @page.user_allowed?(current_refinery_user) &&
-      (@page.try(:live?) ||
-          (refinery_user? and current_refinery_user.authorized_plugins.include?("refinery_pages")))
-
-      # if the admin wants this to be a "placeholder" page which goes to its first child, go to that instead.
-      if @page.skip_to_first_child and (first_live_child = @page.children.order('lft ASC').where(:draft=>false).first).present?
-        redirect_to first_live_child.url
-      end
-    else
-      # redirect to the right login page...
-      redirect_to login_members_path(:redirect => request.fullpath, :member_login => true)
+  def restrict_by_role
+    unless @page.user_allowed?(current_refinery_user)
+      redirect_to '/', :flash => {:erro => "You don't have permissions to view #{@page.slu}"}
     end
+  end
+
+  # def show
+  #   # Find the page by the newer 'path' or fallback to the page's id if no path.
+  #   @page = Refinery::Page.find(params[:path] ? params[:path].to_s.split('/').last : params[:id],
+  #     :include => :roles)
+
+  #   if @page.user_allowed?(current_refinery_user) &&
+  #     (@page.try(:live?) ||
+  #         (refinery_user? and current_refinery_user.authorized_plugins.include?("refinery_pages")))
+
+  #     # if the admin wants this to be a "placeholder" page which goes to its first child, go to that instead.
+  #     if @page.skip_to_first_child and (first_live_child = @page.children.order('lft ASC').where(:draft=>false).first).present?
+  #       redirect_to first_live_child.url
+  #     end
+  #   else
+  #     # redirect to the right login page...
+  #     redirect_to login_members_path(:redirect => request.fullpath, :member_login => true)
+  #   end
   end
 end # PagesController.class_eval

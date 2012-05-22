1ad750f9a77ac495b9607e4fc8628d05796ceb2f
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-05-22 15:41:06 -0700

    commented out more monkeypatches to refinery admin: user and dashboard controllers

--- app/decorators/controllers/refinery/admin/dashboard_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/dashboard_controller_decorator.rb
@@ -1,12 +1,12 @@
 # this broke as part of config.to_prepare
 # Do this or lost password non-admins still goes to the dashboard
-::Refinery::Admin::DashboardController.class_eval do
-  old_index = instance_method(:index)
-  define_method(:index){|*args|
-    if current_refinery_user.has_role?(:super_user) || current_refinery_user.has_role?(:refinery)
-      old_index.bind(self).call(*args)
-    else
-      redirect_to '/'
-    end
-  }
-end
+# ::Refinery::Admin::DashboardController.class_eval do
+#   old_index = instance_method(:index)
+#   define_method(:index){|*args|
+#     if current_refinery_user.has_role?(:super_user) || current_refinery_user.has_role?(:refinery)
+#       old_index.bind(self).call(*args)
+#     else
+#       redirect_to '/'
+#     end
+#   }
+# end
--- app/decorators/controllers/refinery/admin/users_controller_decorator.rb
+++ app/decorators/controllers/refinery/admin/users_controller_decorator.rb
@@ -1,8 +1,8 @@
 # show only admins in Users administration
-::Refinery::Admin::UsersController.class_eval do
-	def index
-		find_all_users ["membership_level <> ?", 'Member']
-		paginate_all_users
-		render :partial => 'users' if request.xhr?
-	end
-end
+# ::Refinery::Admin::UsersController.class_eval do
+# 	def index
+# 		find_all_users ["membership_level <> ?", 'Member']
+# 		paginate_all_users
+# 		render :partial => 'users' if request.xhr?
+# 	end
+# end

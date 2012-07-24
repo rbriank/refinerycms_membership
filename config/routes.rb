bfa2e4fbbc6b4e84d96c87fc0a92cca216ae8a57
Juozas Gaigalas <juozasgaigalas@gmail.com>
2012-07-23 18:35:14 -0700

    Clean up members frontend routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,21 @@ Refinery::Core::Engine.routes.draw do
   scope :module => :memberships do
 
     # Frontend routes
-    resource :members, :except => [:destroy] do
-      get :login
-      #match '/new/welcome' => 'members#welcome', :as => :welcome
-      get :welcome
-      get :new_password
-      match '/new_password' => 'members#create_password', :as => :create_password
-      match '/new_password/created' => 'members#new_password_created', :as => :new_password_created
-      get :profile
-      match '/activate/:confirmation_token' => 'members#activate', :as => :activate, :constraints => {:confirmation_token => /[a-zA-Z0-9]+/}, :via => :get
+    resources :members, :except => [:destroy] do
+      collection do
+        get :login
+        #match '/new/welcome' => 'members#welcome', :as => :welcome
+        get :welcome
+        get :new_password
+        match '/new_password' => 'members#create_password', :as => :create_password
+        match '/new_password/created' => 'members#new_password_created', :as => :new_password_created
+        get :profile
+        match '/activate/:confirmation_token' => 'members#activate', :as => :activate, :constraints => {:confirmation_token => /[a-zA-Z0-9]+/}, :via => :get
+      end
+
     end
+
+
 
     # Admin routes
     namespace :admin, :path => 'refinery/memberships' do

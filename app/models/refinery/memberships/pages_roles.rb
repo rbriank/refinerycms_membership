619d3b01408fcda2988b70427d5a4c9b3d9570fb
Pedro Pablo Guijarro <pedro@programatica.es>
2012-09-05 14:06:36 +0200

    fix page_roles

--- app/models/refinery/memberships/pages_roles.rb
+++ app/models/refinery/memberships/pages_roles.rb
@@ -1,7 +1,7 @@
 module Refinery
   module Memberships
     class PagesRoles <  Refinery::Core::BaseModel
-      self.table_name = 'pages_roles'
+      self.table_name = 'refinery_pages_roles'
       belongs_to :roles
       belongs_to :pages
     end

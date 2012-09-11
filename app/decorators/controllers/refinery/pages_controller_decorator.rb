dc0044abc0f24df4033c7248f8eeb88d97c446cf
Sam Beam <sbeam@onsetcorps.net>
2012-09-11 12:30:44 -0400

    PagesController decorator cleaned old commented code #show

--- app/decorators/controllers/refinery/pages_controller_decorator.rb
+++ app/decorators/controllers/refinery/pages_controller_decorator.rb
@@ -1,29 +1,10 @@
-::Refinery::PagesController.class_eval do
+Refinery::PagesController.class_eval do
   before_filter :restrict_by_role, :only => :show
 
   def restrict_by_role
     unless @page.user_allowed?(current_refinery_user)
-      #redirect_to '/', :flash => {:error => "You don't have permissions to view #{@page.slug}"}
       redirect_to login_members_path(:redirect => request.fullpath, :member_login => true)
     end
   end
 
-  # def show
-  #   # Find the page by the newer 'path' or fallback to the page's id if no path.
-  #   @page = Refinery::Page.find(params[:path] ? params[:path].to_s.split('/').last : params[:id],
-  #     :include => :roles)
-
-  #   if @page.user_allowed?(current_refinery_user) &&
-  #     (@page.try(:live?) ||
-  #         (refinery_user? and current_refinery_user.authorized_plugins.include?("refinery_pages")))
-
-  #     # if the admin wants this to be a "placeholder" page which goes to its first child, go to that instead.
-  #     if @page.skip_to_first_child and (first_live_child = @page.children.order('lft ASC').where(:draft=>false).first).present?
-  #       redirect_to first_live_child.url
-  #     end
-  #   else
-  #     # redirect to the right login page...
-  #     redirect_to login_members_path(:redirect => request.fullpath, :member_login => true)
-  #   end
-  # end
-end # PagesController.class_eval
+end
